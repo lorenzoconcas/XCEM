0f02abc099ef539b01144b298f9f6ea6f724b75a
Lorenzo Concas <35010999+lorenzoconcas@users.noreply.github.com>
2022-02-21 10:56:41 +0100

    base upload

---
+++ application/android/app/src/main/kotlin/com/loreconcas/xcem/application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.loreconcas.xcem.application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ application/lib/models/application_version.dart
@@ -0,0 +1,3 @@
+class ApplicationVersion{
+  ApplicationVersion();
+}
